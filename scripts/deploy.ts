6f32e2031fd15aca8aaef259c06c9b6c29bc78af
hymns12 <nickcloudsyinka1@gmail.com>
2024-02-28 02:54:09 -0500

    updata3

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,22 +1,20 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const NFT = await ethers.deployContract("NFT");
+  await NFT.waitForDeployment();
+  
+  console.log(`decentralized-social-media.sol ${NFT.target}`);
+  
+  const Relayer = await ethers.deployContract("Relayer");
+  await Relayer.waitForDeployment();
+  
+  console.log(`decentralized-social-media.sol ${Relayer.target}`);
+  
+  const socialMedia = await ethers.deployContract("SocialMedia");
+  await socialMedia.waitForDeployment();
 
-  const lockedAmount = ethers.parseEther("0.001");
-
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  console.log(`decentralized-social-media.sol ${socialMedia.target}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
